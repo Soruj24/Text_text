f244f9fb7f420a60a1420a5c4ef552c0a3fe9679
Soruj Mahmud <113199659+Soruj24@users.noreply.github.com>
2024-12-20 08:39:53 +0600

    add text 2

--- name-of-your-project/src/App.jsx
+++ name-of-your-project/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     <>
       <h2>Hello Word</h2>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum quae excepturi tempora nobis quaerat reprehenderit earum, sapiente natus commodi veniam iste nihil veritatis alias laborum ipsa autem hic! Quisquam, odio?</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis ducimus officia, molestias quod beatae quibusdam iure quae accusantium inventore cupiditate, sunt expedita reprehenderit. Delectus nam, facilis aliquid ratione esse aperiam.</p>
     </>
   )
 }
