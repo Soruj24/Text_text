4f58295cce4877b7467dee6fd657fe31b633c149
Soruj Mahmud <113199659+Soruj24@users.noreply.github.com>
2024-12-20 09:00:00 +0600

    add text 4

--- name-of-your-project/src/App.jsx
+++ name-of-your-project/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h2>Hello Word</h2>
+      <h2>Hello Word from React</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse dolorum illum necessitatibus cupiditate obcaecati. Dicta repudiandae cumque asperiores fugiat repellat maxime vitae natus nulla officia nam delectus, similique inventore deleniti.</p>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum quae excepturi tempora nobis quaerat reprehenderit earum, sapiente natus commodi veniam iste nihil veritatis alias laborum ipsa autem hic! Quisquam, odio?</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis ducimus officia, molestias quod beatae quibusdam iure quae accusantium inventore cupiditate, sunt expedita reprehenderit. Delectus nam, facilis aliquid ratione esse aperiam.</p>
